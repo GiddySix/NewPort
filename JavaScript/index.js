fd237f6fffb6fc3ab4b1423a485f6c981f6c299b
Sam <sam.dunlap90@gmail.com>
2022-10-23 16:23:15 -0400

    Remove JavaScript

--- JavaScript/index.js
+++
@@ -1,9 +0,0 @@
-let firstAnswer  = window.prompt('You wake up from stange dream to an even stranger message. Do you answer it, y/n??')
-if(firstAnswer === 'y'){
-    let secondAnswer = window.prompt(`They seem to know you and your mind, they then warned of a knock coming. You open the door and see a chance to meet the strange messager, do you go?y/n`)
-}
-
-else if(firstAnswer === 'n') {
-    let secondAnswer = window.prompt(`You go back to sleep no more adventure`)
-
-}
